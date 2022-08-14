138a9152a194dabf5cf0233d61acf039457ce057
joao.correia <joao.correia.marcos@gmail.com>
2022-08-14 17:27:20 -0300

    add: first controller

---
+++ exemplos/JdbcTemplateExercicio/jdbcTemplateExercicio/src/main/java/br/com/mentoria/controller/ProdutoController.java
@@ -0,0 +1,28 @@
+package br.com.mentoria.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mentoria.dao.ProdutoRepository;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<?> getAllProdutosByPageable(@RequestParam(value = "page") Integer page, @RequestParam(value = "size") Integer size,
+			@RequestParam(value = "orderBy", defaultValue = "ID") String orderBy, @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+		
+		var produtos = repo.findAll(PageRequest.of(page, size));
+		return ResponseEntity.ok().body(produtos); 
+	}
+	
+}
